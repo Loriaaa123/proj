5ceab03c291083a9a7b77b76de1f07838f5c7352
Dloria <dloria@qarva.com>
2022-03-07 15:41:37 +0400

    password reset

--- ParkCommerce/projects/urls.py
+++ ParkCommerce/projects/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.projects, name='projects'),
+    path('', views.projects, name='projects'),
     path('project/<str:project_id>', views.project, name='project'),
     path('create-project/', views.createProject, name='create-project'),
     path('update-project/<str:object_id>', views.updateProject, name='update-project'),
