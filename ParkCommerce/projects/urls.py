14003889d39eb58f7f4b20776a6a1bb25e3dd675
Dloria <dloria@qarva.com>
2022-03-07 17:51:35 +0400

    started rest framework

--- ParkCommerce/projects/urls.py
+++ ParkCommerce/projects/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name='projects'),
-    path('project/<str:project_id>', views.project, name='project'),
+    path('projects/<str:project_id>', views.project, name='project'),
     path('create-project/', views.createProject, name='create-project'),
     path('update-project/<str:object_id>', views.updateProject, name='update-project'),
     path('delete-project/<str:object_id>', views.deleteProject, name='delete-project'),
